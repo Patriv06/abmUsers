d8bbb406d2cd3f870c47020d4526964a471ec2f9
Patriv06 <95047619+Patriv06@users.noreply.github.com>
2022-09-11 11:46:22 -0300

    abm

---
+++ src/app/users/users.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Users } from './users';
+import { HttpClient } from '@angular/common/http';
+//hay que importar import { HttpClientModule } from '@angular/common/http'en el app.module.ts
+//y hay que incluirlo en ls imports:[HttpClientModule]
+@Injectable({
+  providedIn: 'root'
+})
+// hay que importar observable, Httpclient y el modelo (users)
+export class UsersService {
+
+  constructor(private httpClient:HttpClient) { }
+//dirección del back
+  private baseURL="https://senioritty-demo.cleverapps.io/users";
+  usr = {
+    id:" ",
+    nombre:" ",
+    email:" ",
+    
+  }
+  obtenerUsers():Observable<Users[]>{
+   
+    
+    return this.httpClient.get<Users[]>(`${this.baseURL}`);
+    
+       
+  }
+
+  modificarUsers( users:Users) { 
+   
+   return this.httpClient.put<Users>(`${this.baseURL}`, Users)
+
+  
+  }
+
+  borrarUsers(users:Users){
+   
+   return this.httpClient.delete<Users>(this.baseURL+ users.id)
+
+
+
+   
+  }
+  crearUsers(users:Users){
+   
+    return this.httpClient.post<Users>(`${this.baseURL}`, users)
+  
+  }
+
+}
+
+
+
+
+
